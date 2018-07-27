702751241985a39e5b2f6b059cc6c5d33fcb9b4b
Stuart Black <stuartblack@gmail.com>
2018-07-28 00:01:27 +0100

    proper global variables

--- app.rb
+++ app.rb
@@ -6,8 +6,8 @@ require_relative('controllers/recipe_controller')
 
 
 get '/' do
-    @@patron_length = Patron.show_all
-    @@review_length = Review.show_all
-    @@recipe_length = Recipe.show_all
+    $patron_length = Patron.show_all
+    $review_length = Review.show_all
+    $recipe_length = Recipe.show_all
     erb( :index)
 end
